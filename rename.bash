f3f67038296a72a650967f80501b307cd21e6044
Joey Yakimowich-Payne <jyapayne@gmail.com>
2022-07-12 13:28:38 -0600

    Modify rename script

--- rename.bash
+++ rename.bash
@@ -3,7 +3,7 @@ set -e
 PROJECT_NAME="$1"
 echo $PROJECT_NAME
 
-sed -i.bak "s/nimtemplate/$PROJECT_NAME/g" tests/test.nim src/nimtemplate.nim
+sed -i.bak "s/nimtemplate/$PROJECT_NAME/g" tests/test.nim src/nimtemplate.nim nimtemplate.nimble
 
 mv src/nimtemplate src/$PROJECT_NAME
 mv src/nimtemplate.nim src/$PROJECT_NAME.nim
